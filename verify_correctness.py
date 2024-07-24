b265401da7334be3280043eed9a8b231788ad0bf
VaradK62442 <varadkulk123@gmail.com>
2024-07-24 17:29:23 +0100

    add small qof

--- verify_correctness.py
+++ verify_correctness.py
@@ -5,13 +5,15 @@ from enumerateSMs import ESMS
 import math
 from pprint import pprint as pp
 from matplotlib import pyplot as plt
+from tqdm import tqdm
 
 
 class VerifyCorrectness:
-    def __init__(self, total_students, lower_project_bound, upper_project_bound):
+    def __init__(self, total_students, lower_project_bound, upper_project_bound, write_to_file):
         self._total_students = total_students
         self._lower_project_bound = lower_project_bound
         self._upper_project_bound = upper_project_bound
+        self._write_to_file = write_to_file
 
         self._default_filename = 'instance.txt'
         self._results_dir = 'results/'
@@ -42,27 +44,29 @@ class VerifyCorrectness:
 
     def run(self):
         def write_msg_to_file(msg):
-            with open(self._results_dir + self._results_filename, 'a') as f:
-                f.write(msg)
+            if self._write_to_file:
+                with open(self._results_dir + self._results_filename, 'a') as f:
+                    f.write(msg)
 
         s = self.generate_instances()
         if self.verify_instance():
-            write_msg_to_file("Instance occurs last in list of all matchings\n")
             self._correct_count += 1
+            write_msg_to_file(f"PASS: Instance occurs last in list of all matchings\n")
         else:
-            write_msg_to_file("Instance does not occur in list of all matchings\n")
             self._incorrect_count += 1
-            s.write_instance_no_ties(f"{self._results_dir}incorrect_instance_{self._incorrect_count}.txt")
+            write_msg_to_file(f"FAIL: Instance does not occur in list of all matchings - {self._incorrect_count}\n")
+            if self._write_to_file:
+                s.write_instance_no_ties(f"{self._results_dir}incorrect_instance_{self._incorrect_count}.txt")
     
 
     def show_results(self):
         print(f"Correct: {self._correct_count}\nIncorrect: {self._incorrect_count}")
 
-        _, ax = plt.subplots()
-        ax.bar(['Correct', 'Incorrect'], (
-            self._correct_count, self._incorrect_count
-        ))
-        plt.show()
+        # _, ax = plt.subplots()
+        # ax.bar(['Correct', 'Incorrect'], (
+        #     self._correct_count, self._incorrect_count
+        # ))
+        # plt.show()
 
 
 def main():
@@ -70,10 +74,12 @@ def main():
     LOWER_PROJECT_BOUND = 2
     UPPER_PROJECT_BOUND = 3
     REPETITIONS = 1_000
+    WRITE_TO_FILE = False
+
     assert UPPER_PROJECT_BOUND <= int(math.ceil(0.5 * TOTAL_STUDENTS)), "Upper project bound is too high"
 
-    v = VerifyCorrectness(TOTAL_STUDENTS, LOWER_PROJECT_BOUND, UPPER_PROJECT_BOUND)
-    for _ in range(REPETITIONS):
+    v = VerifyCorrectness(TOTAL_STUDENTS, LOWER_PROJECT_BOUND, UPPER_PROJECT_BOUND, WRITE_TO_FILE)
+    for _ in tqdm(range(REPETITIONS)):
         v.run()
 
     v.show_results()
@@ -81,8 +87,9 @@ def main():
 
 if __name__ == '__main__':
     main()
-    # e = ESMS("?.txt")
+    # e = ESMS("instances/test.txt")
     # e.choose(1)
+    # pp(e.all_matchings)
     # s = SPALLecturerOptimal("?.txt")
     # pp(e.all_matchings[-1])
     # pp(s.run())
